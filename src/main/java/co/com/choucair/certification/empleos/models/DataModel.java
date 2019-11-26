b5db246213607c5536322e9f3815e8d235d7565e
reyjach <reymuerto14@hotmail.com>
2019-11-26 18:08:35 -0500

    se agrego la carpeta models y el archivo Datamodel para crear los datos de las tablas

---
+++ src/main/java/co/com/choucair/certification/empleos/models/DataModel.java
@@ -0,0 +1,64 @@
+package co.com.choucair.certification.empleos.models;
+
+public class DataModel {
+    String nombreEmpleo;
+    String Ubicacion;
+    String nombre;
+    String correo;
+    String celular;
+    String estudiosFormales;
+    String experiencia;
+    String conoceAutomatizacion;
+    String aspiracionSalarial;
+    String disponibilidadIngresar;
+    String mensajeAdicional;
+    String hojaVida;
+
+    public String getNombreEmpleo() {
+        return nombreEmpleo;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public String getEstudiosFormales() {
+        return estudiosFormales;
+    }
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public String getConoceAutomatizacion() {
+        return conoceAutomatizacion;
+    }
+
+    public String getAspiracionSalarial() {
+        return aspiracionSalarial;
+    }
+
+    public String getDisponibilidadIngresar() {
+        return disponibilidadIngresar;
+    }
+
+    public String getMensajeAdicional() {
+        return mensajeAdicional;
+    }
+
+    public String getHojaVida() {
+        return hojaVida;
+    }
+}
